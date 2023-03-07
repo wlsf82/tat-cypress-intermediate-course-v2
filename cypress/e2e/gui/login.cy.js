51a51909e22d2bc4996b7c74a180e21f25092142
Walmyr <wlsf82@gmail.com>
2023-03-07 01:25:53 +0100

    Implement project's final solution

---
+++ cypress/e2e/gui/login.cy.js
@@ -0,0 +1,9 @@
+describe("Login", () => {
+  it("successfully", () => {
+    const user = Cypress.env("user_name")
+    const password = Cypress.env("user_password")
+    const options = { cacheSession: false }
+
+    cy.login(user, password, options)
+  })
+})
